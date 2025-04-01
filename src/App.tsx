2aeb11007c5745a719379a2af193ced4dc871960
vincenttucci <vincenttucci66@gmail.com>
2025-04-01 17:47:34 -0400

    Added Night Mode functionality and button

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ if (prevKey !== null) {
 
 function App() {
   const [key, setKey] = useState<string>(keyData); //for api key input
+  const [nightMode, setNightMode] = useState<boolean>(false); // for checking if night mode is active
   
   //sets the local storage item to the api key the user inputed
   function handleSubmit() {
@@ -24,19 +25,32 @@ function App() {
   function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
     setKey(event.target.value);
   }
+
+  // Sets Night Mode on and off
+  function nightModeButton(){
+    setNightMode(!nightMode);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <Form>
-        <Form.Label>API Key:</Form.Label>
-        <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
-        <br></br>
-        <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
-      </Form>
+    <div className={`App-wrapper ${nightMode ? 'night-mode' : ''}`}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <button className='Basic-Questions'>Basic Questions</button>
+        <div>A more comprehensive quiz for students who want a more detailed response</div>
+        <button className='Detailed-Questions'>Detailed Questions</button>
+        <div>An easier quiz for students with less time</div>
+        <Form>
+          <Form.Label>API Key:</Form.Label>
+          <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
+          <br />
+          <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
+        </Form>
+      </div>
       <footer className="App-footer">
-        <p>Names</p>
+        <button onClick={nightModeButton}>Toggle Night Mode</button>
+        <p>Vincent Tucci, Brooklyn Harden, Taylor Jenkins, Sam Mullaney</p>
       </footer>
     </div>
   );
