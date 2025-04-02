b0b05cb50c668fae63882461c3538ad4558be39a
Brooklyn Harden <bharden@udel.edu>
2025-04-01 20:51:54 -0400

    Started the Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
+import HomePage from "./HomePage";
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
@@ -13,6 +14,7 @@ if (prevKey !== null) {
 
 function App() {
   const [key, setKey] = useState<string>(keyData); //for api key input
+  const [currentPage, setCurrentPage] = useState<string>('home'); //this manages home page naviagation
   
   //sets the local storage item to the api key the user inputed
   function handleSubmit() {
@@ -24,10 +26,28 @@ function App() {
   function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
     setKey(event.target.value);
   }
+
+
+  function renderPage(){
+    if (currentPage === 'home'){
+      return <HomePage navigateTo={setCurrentPage}/>;
+    }else
+      return <div>404 Page Not Found</div>
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <Button onClick={() => setCurrentPage('home')}>Home</Button>
+        <br/><br /> */}
+
+        <Form>
+        <Form.Label>API Key:</Form.Label>
+        <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
+        <br></br>
+        <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
+      </Form>
+
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -40,14 +60,19 @@ function App() {
           Learn React
         </a>
         <p>Vincent Tucci</p>
-        <p>, Brooklyn Harden</p>
+        <p>, Brooklyn Harden</p> */}
       </header>
-      <Form>
+
+      {/*Page content rendered here*/}
+      <main>
+        {renderPage()}
+      </main>
+      {/* <Form>
         <Form.Label>API Key:</Form.Label>
         <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
         <br></br>
         <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
-      </Form>
+      </Form> */}
     </div>
   );
 }
